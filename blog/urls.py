84316471f4bf699bc5ac99d6c4c1df9922727892
yusuke30323 <ykonokono19970508@gmail.com>
2020-04-02 11:58:45 +0900

    c

--- blog/urls.py
+++ blog/urls.py
@@ -18,13 +18,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.urls import re_path
-from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 if settings.DEBUG:
