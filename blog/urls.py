37711c420b24c8e6c169ed946909b17c7ab01ae3
yusuke30323 <ykonokono19970508@gmail.com>
2020-04-02 11:40:15 +0900

    c

--- blog/urls.py
+++ blog/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('', include('website.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
